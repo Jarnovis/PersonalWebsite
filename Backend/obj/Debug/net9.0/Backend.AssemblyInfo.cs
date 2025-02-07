d6cbbd0a02753c6ad30ee9d3ac348d0999310c69
Your Name <jd.vis@hotmail.nl>
2025-02-07 22:49:18 +0100

    Program move

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b87880ea2fc873b924291d338508b7a2cef393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ed5b098d76fb9bd13df52c223b71030cc5e30e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
