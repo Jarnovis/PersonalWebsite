e1b87880ea2fc873b924291d338508b7a2cef393
Your Name <jd.vis@hotmail.nl>
2025-02-07 22:40:00 +0100

    update

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353283ebe62aa8bcd3d4d0e573616e1e0d2b226e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a7cceeab9b09be0a97fc715ee13a85f3af0541")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/obj/Release/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2194ffbade262c7e4745d36d03d56ac9862ee695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a7cceeab9b09be0a97fc715ee13a85f3af0541")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
