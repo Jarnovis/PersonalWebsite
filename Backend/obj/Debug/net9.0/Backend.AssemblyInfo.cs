7d49dcc05edf5810507009a1eb603a6ff8dfbd8b
Your Name <jd.vis@hotmail.nl>
2025-02-08 22:44:31 +0100

    rebuild

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d136d54e81e8bc44374285d3307f80a2311238ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b948c6eb4bbc0f049ae5ab0ae12f80060f0105b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
