ee2613af251f355bfce7d894484c45336d98262d
Your Name <jd.vis@hotmail.nl>
2025-02-08 21:36:20 +0100

    argument

--- Backend/src/StudyInfo/OpenWebsite.cs
+++ Backend/src/StudyInfo/OpenWebsite.cs
@@ -139,7 +139,7 @@ public class OpenWebsite : IDisposable
         {
             // Ubuntu/Docker (Linux-based system)
             options.BinaryLocation = "/usr/bin/chromium";
-            options.AddArguments("--headless", "--disable-gpu", "--remote-debugging-port=9222", "--no-sandbox");
+            options.AddArguments("--headless", "--disable-gpu", "--remote-debugging-port=9222", "--no-sandbox", "--disable-dev-shm-usage");
         }
         else if (System.Environment.OSVersion.Platform == PlatformID.Win32NT)
         {
