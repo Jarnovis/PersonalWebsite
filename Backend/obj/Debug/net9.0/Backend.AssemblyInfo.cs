3eb1d3581c4592310a721ddb33c5ae07a290332d
Your Name <jd.vis@hotmail.nl>
2025-02-08 17:11:21 +0100

    No more analyzers

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967f8f4348b81b31d537b290f4db2ab7f773eb88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb3c89aad8df58c8606e5b27c30702314b33dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
