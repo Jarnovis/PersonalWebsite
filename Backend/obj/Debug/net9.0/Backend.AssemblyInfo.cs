49c1ad16a54d9e6cc12043b40597b469b937d962
Your Name <jd.vis@hotmail.nl>
2025-02-08 17:03:32 +0100

    restore

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089ffe786f5878b02e28fb0ca0db88e524bc2e47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d55fb3d8240d7333b10e5cf99372a9c77628d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
